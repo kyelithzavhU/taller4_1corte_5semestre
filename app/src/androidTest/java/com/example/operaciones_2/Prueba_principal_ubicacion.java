f29930152f27e369c17ab3968d11fc14438dbbc6
kyelithzavhU <karen.vargash@uniagustiniana.edu.co>
2023-09-03 20:39:02 -0500

    Se agrega test de git action

---
+++ app/src/androidTest/java/com/example/operaciones_2/Prueba_principal_ubicacion.java
@@ -0,0 +1,112 @@
+package com.example.operaciones_2;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class Prueba_principal_ubicacion {
+
+    @Rule
+    public ActivityScenarioRule<pantalla_principal> mActivityScenarioRule =
+            new ActivityScenarioRule<>(pantalla_principal.class);
+
+    @Test
+    public void prueba_principal_ubicacion() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.campo_op),
+                        childAtPosition(
+                                allOf(withId(R.id.ingrese_op),
+                                        childAtPosition(
+                                                withId(R.id.pant_prin),
+                                                2)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.campo_op),
+                        childAtPosition(
+                                allOf(withId(R.id.ingrese_op),
+                                        childAtPosition(
+                                                withId(R.id.pant_prin),
+                                                2)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("4"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.campo_op), withText("4"),
+                        childAtPosition(
+                                allOf(withId(R.id.ingrese_op),
+                                        childAtPosition(
+                                                withId(R.id.pant_prin),
+                                                2)),
+                                0),
+                        isDisplayed()));
+        appCompatEditText3.perform(pressImeActionButton());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.boton_op), withText("Ingresar"),
+                        childAtPosition(
+                                allOf(withId(R.id.ingrese_op),
+                                        childAtPosition(
+                                                withId(R.id.pant_prin),
+                                                2)),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
